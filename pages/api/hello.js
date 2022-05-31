9f93aa8446fad5c102dae030cc4ddf914d7af900
gonzaotc <gonza.otc@gmail.com>
2022-05-31 12:03:55 -0300

    test branch push

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+useEffect(() => {
+  let buyMeACoffee;
+  isWalletConnected();
+  getMemos();
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  const onNewMemo = (from, timestamp, name, message) => {
+    setMemos(prevState => [
+      ...prevState,
+      {
+        address: from,
+        timestamp: new Date(timestamp * 1000),
+        message: message,
+        name: name,
+      },
+    ]);
+  };
+
+  const { ethereum } = window;
+
+  // Listen for new memo events.
+  if (ethereum) {
+    const provider = new ethers.providers.Web3Provider(ethereum, "any");
+    const signer = provider.getSigner();
+    buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer);
+
+    buyMeACoffee.on("NewMemo", onNewMemo);
+  }
+
+  return () => {
+    if (buyMeACoffee) {
+      buyMeACoffee.off("NewMemo", onNewMemo);
+    }
+  };
+}, []);
